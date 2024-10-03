b4f33fda29d033bde20089b093fe6bf57a6228dc
magistraapta <magsitraagis@gmail.com>
2024-10-03 19:57:46 +0800

    modify user struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type User struct {
-	Id      int    `json:"id" db:"id"`
-	Name    string `json:"name" db:"name"`
-	Phone   string `json:"phone" db:"phone"`
-	Address string `json:"address" db:"address"`
+	Id        int       `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
