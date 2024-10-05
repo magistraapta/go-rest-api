0dfdd6eda6ad757f60c952758d113de7d1ce7aa9
magistraapta <magsitraagis@gmail.com>
2024-10-05 14:08:32 +0800

    add middleware

--- internal/api/middleware/auth.go
+++ internal/api/middleware/auth.go
@@ -1,38 +1,25 @@
 package middleware
 
 import (
-	"encoding/base64"
+	"example/hello/internal/api/handlers"
 	"net/http"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
-func AuthMiddleware(username, password string) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			auth := c.Request().Header.Get("Authorization")
-
-			if auth == "" {
-				return c.String(http.StatusUnauthorized, "Unautorized")
-			}
-
-			if !strings.HasPrefix(auth, "Basic") {
-				return c.String(http.StatusUnauthorized, "Unauthorized")
-			}
-
-			payload, err := base64.StdEncoding.DecodeString(strings.TrimPrefix(auth, "Basic"))
-
-			if err != nil {
-				return c.String(http.StatusUnauthorized, "Unauthorized")
-			}
-
-			parts := strings.SplitN(string(payload), ":", 2)
-			if len(parts) != 2 || parts[0] != username || parts[1] != password {
-				return c.String(http.StatusUnauthorized, "Unauthorized")
-			}
-
-			return next(c)
+func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("Token")
+		if err != nil {
+			return c.Redirect(http.StatusSeeOther, "/login")
 		}
+
+		claims, err := handlers.VerifyToken(cookie.Value)
+		if err != nil {
+			return c.Redirect(http.StatusSeeOther, "/login")
+		}
+
+		c.Set("user", claims.Username)
+		return next(c)
 	}
 }
