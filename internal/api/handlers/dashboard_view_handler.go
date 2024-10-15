43382362cc99bc8f70abce81b190a0b650ea0c20
magistraapta <magsitraagis@gmail.com>
2024-10-15 20:00:03 +0800

    add post and user

--- internal/api/handlers/dashboard_view_handler.go
+++ internal/api/handlers/dashboard_view_handler.go
@@ -10,19 +10,28 @@ import (
 
 type DashboardPageData struct {
 	Title string
+	Posts []models.Post
 	Users []models.User
 }
 
 func DashboardViewHandler(c echo.Context) error {
+	var posts []models.Post
 	var users []models.User
 
-	err := db.DB.Select(&users, "SELECT * FROM users")
-	if err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+	err_post := db.DB.Select(&posts, "SELECT * FROM posts")
+	if err_post != nil {
+		return c.String(http.StatusInternalServerError, err_post.Error())
 	}
+
+	err_user := db.DB.Select(&users, "SELECT * FROM users")
+	if err_user != nil {
+		return c.String(http.StatusInternalServerError, err_user.Error())
+	}
+
 
 	data := DashboardPageData{
 		Title: "Dashboard",
+		Posts: posts,
 		Users: users,
 	}
 	return c.Render(http.StatusOK, "dashboard", data)
